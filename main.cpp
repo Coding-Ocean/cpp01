31d620ca733f7c1bc6e358b2daee917fde6506f9
Coding-Ocean <eijo524@gmail.com>
2023-05-19 15:11:30 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,110 @@
+//基本型。auto。コンソールアウトプット。
+#if 0
+#include<iostream>
+int main()
+{
+
+    system("pause>0");
+}
+#endif
+//{}で初期化
+#if 0
+#include<iostream>
+int main()
+{
+
+    system("pause>0");
+}
+#endif
+//コンソールインプット
+#if 0
+#include <iostream>
+int main()
+{
+
+    system("pause>0");
+}
+#endif
+//名前空間
+#if 0
+#include <iostream>
+int main()
+{
+
+    system("pause>0");
+}
+#endif
+//関数のオーバーロード（引数が違うと、同じ名前の関数を定義できる）
+#if 0
+#include<iostream>
+int main() 
+{
+
+    system("pause>0");
+}
+#endif
+//参照(reference)
+#if 0
+#include<iostream>
+int main()
+{
+
+    system("pause>0");
+}
+#endif
+//配列for
+#if 0
+#include<iostream>
+int main()
+{
+
+    system("pause>0");
+}
+#endif
+//動的確保と解放
+#if 0
+#include<iostream>
+#include<crtdbg.h>
+int main()
+{
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+    system("pause>0");
+}
+#endif
+//string
+#if 0
+#include<iostream>
+int main()
+{
+
+    system("pause>0");
+}
+#endif
+//ファイルストリーム
+#if 0
+#include<iostream>
+int main()
+{
+
+    system("pause>0");
+}
+#endif
+//文字列ストリーム
+#if 0
+#include<iostream>
+int main()
+{
+
+    system("pause>0");
+}
+#endif
+//class
+#if 0
+#include<iostream>
+int main()
+{
+
+    system("pause>0");
+}
+#endif
